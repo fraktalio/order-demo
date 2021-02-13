93da771512719175416495aafc6bc2d88664d819
Ivan Dugalic <ivan.dugalic@axoniq.io>
2021-02-13 19:22:40 +0100

    PropagatingErrorHandler configured
    Errors on event processing side will be propagated to processor which will retry reading the event.

--- src/main/java/com/fraktalio/order/query/configuration/OrderQueryConfiguration.java
+++ src/main/java/com/fraktalio/order/query/configuration/OrderQueryConfiguration.java
@@ -1,5 +1,7 @@
 package com.fraktalio.order.query.configuration;
 
+import org.axonframework.config.EventProcessingConfigurer;
+import org.axonframework.eventhandling.PropagatingErrorHandler;
 import org.axonframework.messaging.interceptors.LoggingInterceptor;
 import org.axonframework.queryhandling.QueryBus;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,5 +18,19 @@ public class OrderQueryConfiguration {
     @Autowired
     public void registerQueryInterceptorsOnBus(QueryBus queryBus) {
         queryBus.registerHandlerInterceptor(new LoggingInterceptor<>());
+    }
+
+    /*******************************************************************************************************************************/
+    /* Register default listener invocation handler on the event processors */
+    /* By default these exceptions are logged and processing continues with the next handler or message */
+    /* We change this behaviour by configuring custom listener PropagatingErrorHandler to re-throw the exception to the processor. */
+
+    /* https://docs.axoniq.io/reference-guide/axon-framework/events/event-processors#exceptions-raised-by-event-handler-methods */
+
+    /*******************************************************************************************************************************/
+
+    @Autowired
+    public void configureErrorHandling(EventProcessingConfigurer config) {
+        config.registerDefaultListenerInvocationErrorHandler(configuration -> PropagatingErrorHandler.instance());
     }
 }
